76fbff6f338eaad627d8da59a8bf515c566b1e03
santisuk5908 <64579242+santisuk5908@users.noreply.github.com>
2020-04-30 18:43:59 +0700

    Add files via upload

---
+++ POS_system/POS_system/Models/Bill_Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS_system.Models
+{
+    public class Bill_Item
+    {
+        public string เลขที่ใบเสร็จ { get; set; }
+        public string ผู้ขาย { get; set; }
+        public string ลูกค้า { get; set; }
+        public string เวลา { get; set; }
+        public string จำนวน { get; set; }
+        public string ราคา { get; set; }
+        public string ส่วนลด { get; set; }
+        public float รวมทั้งสิ้น { get; set; }
+    }
+}
---
+++ POS_system/POS_system/Models/CartItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS_system.Models
+{
+    public class CartItem
+    {
+        public string บาร์โค้ด { get; set; }
+        public string ชื่อสินค้า { get; set; }
+        public string ประเภทสินค้า { get; set; }
+        public int จำนวน { get; set; }
+        public string หน่วยนับ { get; set; }
+        public float ราคาต่อหน่วย { get; set; }
+        public float ราคารวม { get; set; }
+    }
+}
